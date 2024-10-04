09103fe319cb93add5aa612938820f5811cfef5a
Farruh Zoirov <fzoirov29@gmail.com>
2024-10-04 21:43:05 +0500

    commit

---
+++ src/bot/common/enums/purchases.enum.ts
@@ -0,0 +1,16 @@
+export enum Purchases_uz {
+    CREATING_PURCHASE = "Xarid yaratish",
+    PENDING_ORDERS = "Jarayondagi buyurtmalar",
+    CONFIRMED_ORDERS = "Tastiqlangan buyurtmalar",
+    IN_WAY_ORDERS = "Yo'ldagi buyurtmalar",
+    COMPLETED_ORDERS = "Tugallangan buyurtmalar",
+}
+
+
+export enum Purchases_ru {
+    CREATING_PURCHASE = "Создать заказ",
+    PENDING_ORDERS = "Заказы в процессе",
+    CONFIRMED_ORDERS = "Подтвержденные заказы",
+    IN_WAY_ORDERS = "Заказы в пути",
+    COMPLETED_ORDERS = "Завершенные заказы",
+}
